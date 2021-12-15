9210ad3535208aeeef98b07119eb79ef2bda3146
catherinedynda <catherinedynda@users.noreply.github.com>
2021-12-14 22:49:40 -0500

    System page condensed into one, functionality for user admin view, half functionality for vehicle admin view

--- src/router.js
+++ src/router.js
@@ -6,8 +6,13 @@ import SignIn from "./pages/SignIn.vue";
 import SignUp from "./pages/SignUp.vue";
 import About from "./pages/About.vue";
 import System from "./pages/System.vue";
-import Users from "./pages/Users.vue";
-import Passengers from "./pages/Passengers.vue";
+// import Vehicles from "./pages/Vehicles.vue";
+// import VehicleTypes from "./pages/VehicleTypes.vue";
+// import Locations from "./pages/Locations.vue";
+// import Users from "./pages/Users.vue";
+// import Rides from "./pages/Rides.vue";
+// import Drivers from "./pages/Drivers.vue";
+// import Passengers from "./pages/Passengers.vue";
 import ResetPassword from "./pages/ResetPassword.vue";
 
 Vue.use(Router);
@@ -20,9 +25,13 @@ export default new Router({
     { name: "sign-up", path: "/sign-up", component: SignUp },
     { name: "sign-in", path: "/sign-in", component: SignIn },
     { name: "about-us", path: "/about-us", component: About },
-    { name: "system", path: "/system", component: System},
-    { name: "users", path: "/users", component: Users },
-    { name: "passengers", path: "/passengers", component: Passengers },
+    { name: "system", path: "/system", component: System },
+    // { name: "vehicles", path: "/vehicles", component: Vehicles },
+    // { name: "vehicle-types", path: "/vehicle-types", component: VehicleTypes },
+    // { name: "locations", path: "/locations", component: Locations },
+    // { name: "rides", path: "/rides", component: Rides }
+    // { name: "users", path: "/users", component: Users },
+    // { name: "passengers", path: "/passengers", component: Passengers },
     { name: "reset-password", path: "/reset-password", component: ResetPassword}
   ]
 });
